d8f4e107cb10207dfab31055a733cc4ff321768d
Wilberth Gonzalez <wilbertg@uci.edu>
2024-04-28 23:12:17 -0700

    Create script for searching in navbar

---
+++ webapp/scripts/search.js
@@ -0,0 +1,22 @@
+let search_form = $("#search-form");
+
+function submitSearch(searchEvent) {
+    console.log("Search form submitted");
+    searchEvent.preventDefault();
+
+    // Serialize form data into a query string
+    let formData = $(this).serializeArray().filter(function(item) {
+        return item.value.trim() !== ""; // Filter out inputs with empty values
+    });
+
+    let queryString = $.param(formData);
+    if (queryString !== "")
+        queryString = "?" + queryString;
+
+    // Construct the URL with the query string
+    // Redirect to the constructed URL
+    window.location.href = "/Fabflix_war/public/movie-list.html" + queryString;
+}
+
+search_form.submit(submitSearch);
+
