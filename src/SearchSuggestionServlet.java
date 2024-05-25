fb7e60bccfc3ba10bdf38907f0fdb6fea4b4ed6d
Wilberth Gonzalez <wilbertg@uci.edu>
2024-05-24 22:36:11 -0700

    add autocomplete logic without caching

---
+++ src/SearchSuggestionServlet.java
@@ -0,0 +1,80 @@
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+
+@WebServlet(name = "SearchSuggestionServlet", urlPatterns = "/public/api/search-suggestion")
+public class SearchSuggestionServlet extends HttpServlet {
+    private static final long serialVersionUID = 2L;
+    private DataSource dataSource;
+
+    public void init(ServletConfig config) {
+        try {
+            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/moviedb");
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+     * response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("application/json");
+        String query = request.getParameter("query");
+
+        PrintWriter out = response.getWriter();
+
+        JsonArray output = new JsonArray();
+
+        if (query == null) {
+            out.write(output.toString());
+            return;
+        }
+
+        try (out; Connection conn = dataSource.getConnection()) {
+            String[] keywords = query.split("\\s+");
+            StringBuilder booleanQuery = new StringBuilder();
+            for (String keyword : keywords) {
+                booleanQuery.append("+").append(keyword.toLowerCase()).append("* ");
+            }
+            String moviesQuery = "SELECT ft.* FROM moviedb.ft_movie_titles ft " +
+                                "WHERE MATCH (ft.title) AGAINST (? IN BOOLEAN MODE) LIMIT 10";
+            PreparedStatement pstmt = conn.prepareStatement(moviesQuery);
+            pstmt.setString(1, booleanQuery.toString().trim());
+            ResultSet rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                JsonObject movie = new JsonObject();
+                JsonObject data = new JsonObject();
+                movie.addProperty("value", rs.getString("title"));
+                data.addProperty("movie_id", rs.getString("movieID"));
+                movie.add("data", data);
+                output.add(movie);
+            }
+            rs.close();
+            pstmt.close();
+
+            out.write(output.toString());
+            response.setStatus(200);
+        } catch (Exception e) {
+            // Write error message JSON object to output
+            e.printStackTrace();
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("errorMessage", e.getMessage());
+            out.write(jsonObject.toString());
+            response.setStatus(500);
+        }
+    }
+}
--- webapp/scripts/search.js
+++ webapp/scripts/search.js
@@ -1,18 +1,12 @@
+let cachedSuggestions = [];
+
 let adv_search_toggle = $("#adv-search-toggle");
 let adv_search_dropdown = $("#adv-search-dropdown");
 
-// Show ellipsis button when search bar is focused
 $(".nav-title-search-bar").on("focus", function() {
     adv_search_toggle.removeClass("hidden");
 });
 
-// Toggle advanced search dropdown when ellipsis button is clicked
-function showAdvanced() {
-    adv_search_dropdown.toggleClass("hidden");
-    adv_search_dropdown.toggleClass("show");
-}
-
-// Prevent closing dropdown when clicking inside the dropdown
 $(".adv-search-dropdown").on("click", function(event) {
     event.stopPropagation();
 });
@@ -25,18 +19,46 @@ $(".nav-title-search-bar").on("blur", function() {
     $("#nav-search-form").removeClass("nav-focused");
 });
 
+function showAdvanced() {
+    adv_search_dropdown.toggleClass("hidden");
+    adv_search_dropdown.toggleClass("show");
+}
+
+function getSuggestions(query, done) {
+    console.log("Autocomplete search initiated")
+
+    // TODO: if you want to check past query results first, you can do it here
+
+
+    $.ajax({
+        "method": "GET",
+        "url": "/Fabflix/public/api/search-suggestion?query=" + encodeURIComponent(query),
+        "success": function(data) {
+            ajaxSuccessCallback(data, query, done)
+        },
+        "error": function(errorData) {
+            console.log("lookup ajax error")
+            console.log(errorData)
+        }
+    })
+
+}
+
+function ajaxSuccessCallback(data, query, done) {
+    console.log("raw data" + data)
+
+    // TODO: if you want to cache the result into a global variable you can do it here
+
+    done( { suggestions: data } );
+}
+
+function selectSuggestion(suggestion) {
+    console.log("Suggestion selected" + suggestion);
+    window.location.href = "/Fabflix/public/single-movie.html?id=" + suggestion["data"]["movie_id"];
+}
+
 $(document).ready(function() {
     let search_form = $(".search-form");
-
-    // Close dropdown when clicking anywhere else on the website
-    $(document).on("click", function(event) {
-        if (!search_form.is(event.target) && search_form.has(event.target).length === 0) {
-            adv_search_dropdown.addClass("hidden");
-            adv_search_dropdown.removeClass("show");
-            adv_search_toggle.addClass("hidden");
-        }
-    });
-
     function submitSearch(searchEvent) {
         console.log("Search form submitted");
         searchEvent.preventDefault();
@@ -51,6 +73,24 @@ $(document).ready(function() {
             window.location.href = "/Fabflix/public/movie-list.html" + queryString;
         }
     }
-
     search_form.submit(submitSearch);
+
+    $(document).on("click", function(event) {
+        if (!search_form.is(event.target) && search_form.has(event.target).length === 0) {
+            adv_search_dropdown.addClass("hidden");
+            adv_search_dropdown.removeClass("show");
+            adv_search_toggle.addClass("hidden");
+        }
+    });
+
+    $(".full-text-search-bar").autocomplete({
+        lookup: function (query, done) {
+            getSuggestions(query, done);
+        },
+        onSelect: function (suggestion) {
+            selectSuggestion(suggestion);
+        },
+        deferRequestBy: 300, // delay for 300ms between query lookups
+        minChars: 3 // Minimum of 3 characters needed before a lookup is performed
+    });
 });
